5ebcbbd0a036386e916c1a55c274b110fd973b82
FajarPramana <fajarpramana35@gmail.com>
2022-11-28 00:04:16 +0800

    uts_webprog
    uts webprog

---
+++ resources/views/komponen.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title> SKEMA </title>
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+        <!-- content komponen start -->
+        <div class="komponen">KOMPONEN YANG DIPAKAI<br>1. ARDUINO uno<br>2.  small sound sensor<br>3.  lcd.i2c<br>4.  JUMPER WIRE<br>
+            5.  BREADBOARD<br>
+        
+        </div>    
+        <!-- content komponen end -->
+    </body>
+</html>
---
+++ resources/views/skema.blade.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title> SKEMA </title>
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+        <!-- content skema start -->
+        <j class="text-skema">INI ADALAH SKEMA NYA</j>
+        <div class="container-skema">
+            <img src="img/skema.jpeg" class="img-skema"/>       
+        </div>
+        <!-- content 1 end -->
+            </body>
+            
+</html>
